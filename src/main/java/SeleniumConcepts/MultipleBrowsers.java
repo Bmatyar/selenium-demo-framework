b71c3e26573277d62cab4b6545cc50ecd0ebf32f
beantkaur003 <beantkaur003@gmail.com>
2024-02-18 21:13:02 -0500

    Added a tearDown method

--- src/main/java/SeleniumConcepts/MultipleBrowsers.java
+++ src/main/java/SeleniumConcepts/MultipleBrowsers.java
@@ -4,38 +4,38 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.Parameters;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
+
 public class MultipleBrowsers {
+	
+	WebDriver driver = null;
 
 	@Test
-	//@Parameters("browser")
-	public void Openbrowser(String browser) throws Exception
-	{
+	public void Openbrowser() throws Exception {
+		
 		String browser1 = System.getProperty("browser");
-		if(browser1.equalsIgnoreCase("firefox"))
-		{
-	WebDriver driver = new FirefoxDriver();
-	driver.get("https://www.demoblaze.com/");
-		}
-		else if(browser1.equalsIgnoreCase("Chrome"))
-		{
-		WebDriver driver = new ChromeDriver();
-		driver.get("https://www.demoblaze.com/");
-		}
-		else if(browser1.equalsIgnoreCase("Edge"))
-		{
-		WebDriver driver = new EdgeDriver();
-		driver.get("https://www.demoblaze.com/");
-		}
-		else
-		{
+		if (browser1.equalsIgnoreCase("firefox")) {
+			 driver = new FirefoxDriver();
+			driver.get("https://www.demoblaze.com/");
+		} else if (browser1.equalsIgnoreCase("Chrome")) {
+			 driver = new ChromeDriver();
+			driver.get("https://www.demoblaze.com/");
+		} else if (browser1.equalsIgnoreCase("Edge")) {
+			driver = new EdgeDriver();
+			driver.get("https://www.demoblaze.com/");
+		} else {
 			throw new Exception("Browser is not correct");
 		}
 		
-			
-		
+		System.out.println("Page title = "+driver.getTitle());
+
 	}
 	
+	@AfterMethod
+	public void tearDown() {
+		driver.quit();
+	}
+
 }
